68696cf204f8c0792a9ecaf9f4c94db25a881937
banjuede <banjuede@users.noreply.github.com>
2025-03-27 17:41:28 +0800

    a

--- trt-llm/benchmark.sh
+++ trt-llm/benchmark.sh
@@ -2,8 +2,7 @@
   --model deepseek-ai/DeepSeek-V3 \
   --model_path  /userdata/llms/DeepSeek-R1-FP4 \
   throughput \
-  --backend pytorch \
-  --engine_dir /userdata/llms/DeepSeek-R1-FP4 \
+  --backend autodeploy \
   --max_batch_size 161 \
   --max_num_tokens 1160 \
   --dataset /userdata/dataset.txt \
@@ -14,3 +13,6 @@
   --streaming \
   --kv_cache_free_gpu_mem_fraction 0.85 \
   --extra_llm_api_options /userdata/deepseek-test/trt-llm/extra-llm-api-config.yml 2>&1 | tee /userdata/trt_bench.log
+
+
+  #   --engine_dir /userdata/llms/DeepSeek-R1-FP4 \
