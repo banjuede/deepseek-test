47317deb09fd460fb3098d8b2d3f2633e9095018
banjuede <banjuede@users.noreply.github.com>
2025-03-29 03:44:25 +0800

    a

--- trt-llm/serve-test.sh
+++ trt-llm/serve-test.sh
@@ -5,5 +5,21 @@ python /userdata/vllm/benchmarks/benchmark_serving.py \
         --dataset-name sharegpt \
         --dataset-path "/userdata/sharegpt/ShareGPT_V3_unfiltered_cleaned_split.json" \
         --max-concurrency 100 \
-        --num-prompts 3000  \
-        --save-result
+        --num-prompts 3
+
+#!/bin/bash
+# max_concurrency_list=(100 150 200 250 300 350 400 450 500 550 600 650 700 750 800 850 900 950 1000)
+# output_lengths=(8192)
+
+# echo "sharedgpt dataset benchmark"
+
+# for max_concurrency in "${max_concurrency_list[@]}"; do
+#   for output_length in "${output_lengths[@]}"; do
+#     num_prompts=5000
+#     cmd="python3 /userdata/vllm/benchmarks/benchmark_serving.py--backend openai-chat --model /userdata/llms/deepseek-ai/DeepSeek-R1 \
+#       --endpoint /v1/chat/completions --num-prompts $num_prompts --max-concurrency $max_concurrency \
+#       --dataset-path /userdata/sharegpt/ShareGPT_V3_unfiltered_cleaned_split.json"
+#     echo $cmd
+#     eval "$cmd"
+#   done
+# done
