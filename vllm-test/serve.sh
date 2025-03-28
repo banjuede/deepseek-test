a0fa2962968978998daa16b7fbd55a4a0821b91d
banjuede <banjuede@users.noreply.github.com>
2025-03-29 03:15:44 +0800

    a

--- vllm-test/serve.sh
+++ vllm-test/serve.sh
@@ -1,3 +1,3 @@
 export VLLM_TRACE_FUNCTION=1
 
-vllm serve /userdata/llms/deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B --trust-remote-code
+vllm serve /userdata/llms/deepseek-ai/DeepSeek-R1 --trust-remote-code
