11d0f154f28d14b8c588dd428adbab8ee2885909
banjuede <banjuede@users.noreply.github.com>
2025-03-29 03:32:43 +0800

    a

--- trt-llm/serve-test.sh
+++ trt-llm/serve-test.sh
@@ -3,7 +3,7 @@ python vllm/benchmarks/benchmark_serving.py \
         --endpoint /v1/chat/completions \
         --model '/userdata/llms/deepseek-ai/DeepSeek-R1' \
         --dataset-name sharegpt \
-        --dataset-path /userdata/sharegpt/ShareGPT_V3_unfiltered_cleaned_split.json" \
+        --dataset-path "/userdata/sharegpt/ShareGPT_V3_unfiltered_cleaned_split.json" \
         --max-concurrency 100 \
         --num-prompts 3000  \
         --save-result
