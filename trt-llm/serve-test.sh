ca5c7de748a0339c5fef1ff16610aee0dd899bb7
banjuede <banjuede@users.noreply.github.com>
2025-03-29 03:33:28 +0800

    a

--- trt-llm/serve-test.sh
+++ trt-llm/serve-test.sh
@@ -1,4 +1,4 @@
-python vllm/benchmarks/benchmark_serving.py \
+python /userdata/vllm/benchmarks/benchmark_serving.py \
         --backend openai-chat \
         --endpoint /v1/chat/completions \
         --model '/userdata/llms/deepseek-ai/DeepSeek-R1' \
