9ce80560d581942de187bd639b880de0d4aab6a3
banjuede <banjuede@users.noreply.github.com>
2025-03-29 01:38:01 +0800

    a

--- vllm-test/docker-run.sh
+++ vllm-test/docker-run.sh
@@ -1,6 +1,6 @@
 # docker run -d --name trt-llm -e NVIDIA_VISIBLE_DEVICES=all -p 8000:8000 -v /userdata:/userdata tensorrt_llm/release:latest sleep infinity
 
-docker run -d --name triton -it --network host --ipc host --shm-size=32G \
+docker run -d --name lumk -it --network host --ipc host --shm-size=128G \
     --privileged \
     --ulimit nofile=65536:65536 \
     --gpus all \
