263b3e1a530c20d71ebbc1118ad0b4826136281c
banjuede <banjuede@users.noreply.github.com>
2025-04-06 01:07:23 +0800

    a

--- sglang-test/clone.sh
+++ sglang-test/clone.sh
@@ -1,1 +1,7 @@
 git clone https://github.com/sgl-project/sglang.git
+
+# Use the last release branch
+# git clone -b v0.4.4.post3 https://github.com/sgl-project/sglang.git
+
+pip install --upgrade pip
+pip install -e "python[all]" --find-links https://flashinfer.ai/whl/cu124/torch2.5/flashinfer-python
